0a3a92d5a2c945aa3661a56237568387032dd33d
Muhamad Yusof <mhduusop@gmail.com>
2024-12-18 12:12:24 +0700

    feat: setup admin v1.0.0

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
+import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import packageJson from './package.json';
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
-// https://vite.dev/config/
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), nodePolyfills()],
+  base: "/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  define: {
+    'import.meta.env.PACKAGE_VERSION': JSON.stringify(packageJson.version)
+  },
 })
